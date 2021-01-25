ac4e05660eee1ca019dd083bc492cd24badfa5b5
Natalia Anez Safar <nataliaanezsafar@gmail.com>
2021-01-25 19:09:39 -0400

    Base de datos enlazada.

--- PryAnezNatalia/PryAnezNatalia/Models/Test.cs
+++ PryAnezNatalia/PryAnezNatalia/Models/Test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace PryAnezNatalia.Models
     public class Test
     {
         public int id { get; set; }
+        [Key]
         public string name { get; set; }
         public string username { get; set; }
         public string email { get; set; }
