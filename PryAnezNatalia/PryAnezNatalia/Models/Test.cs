e4c3b131921842d5baf21aca1c668a89da594d46
Natalia Anez Safar <nataliaanezsafar@gmail.com>
2021-01-25 19:07:06 -0400

    Modelos creados.

--- PryAnezNatalia/PryAnezNatalia/Models/Test.cs
+++ PryAnezNatalia/PryAnezNatalia/Models/Test.cs
@@ -7,5 +7,13 @@ namespace PryAnezNatalia.Models
 {
     public class Test
     {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public Address address { get; set; }
+        public string phone { get; set; }
+        public string website { get; set; }
+        public Company company { get; set; }
     }
 }
